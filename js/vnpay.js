3a62f20f108caec8b9eaeeaa99dca7f1b50d876e
Quan-2004 <phamhongquan9118@gmail.com>
2026-01-23 20:59:51 +0700

    fix vnpay2

--- js/vnpay.js
+++ js/vnpay.js
@@ -6,7 +6,7 @@
 
 // VNPay Sandbox Configuration
 const vnp_TmnCode = "EQ14MGSD"; // Mã Website Sandbox
-const vnp_HashSecret = "DI2D29AV53QZAYXAWKWURQXFO4ZV3QL5"; // Secret Key Sandbox
+const vnp_HashSecret = "Q72JV4SGW41QBEGY8CKCYD8TEOLL7OYT"; // Secret Key Sandbox
 const vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html"; // URL thanh toán Sandbox
 const vnp_ReturnUrl = window.location.origin + window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/')) + "/index.html";
 
